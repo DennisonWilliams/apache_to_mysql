f0140a1f55edccaf5cbbd97fb9c6c0691d9a9660
radicaldesigns <dev@radicaldesigns.org>
2013-09-13 11:53:12 -0700

    This adds a way to switch on differrent LogFormats from the command line

--- apache_to_mysql.pl
+++ apache_to_mysql.pl
@@ -20,11 +20,12 @@ use POSIX qw(strftime);
 use Getopt::Long;
 
 # global variables
-our ($DBH, $SV, $SERVER, $DATABASE, $USERNAME, $PASSWORD, $VERBOSE, $INTERVAL);
+our ($DBH, $SV, $SERVER, $DATABASE, $USERNAME, $PASSWORD, $VERBOSE, $INTERVAL, $FORMAT);
 $DATABASE = 'apache';
 $USERNAME = 'apache';
 $SERVER = 'localhost';
 $VERBOSE = 1;
+$FORMAT = 'combined-php';
 
 # Interval is the number of minutes in which we will be sending reports
 # and is used by the report function
@@ -41,7 +42,8 @@ my $result = GetOptions (
 	"end=s"      => \$ENDTIME, # string
 	"vhost=s"    => \$VHOST, # string
 	"limit=i"    => \$LIMIT, # integer
-	"report"		 => \$REPORT,
+	"report"     => \$REPORT,
+        "format=s" => \$FORMAT,
 	"verbose"    => \$VERBOSE); # flag
 
 # Schema version.  Increment this each time there is a change to the DB schema,
@@ -65,21 +67,6 @@ elsif ($REPORT) {
 }
 
 # A statement handler to add log entries to the database
-# We will be using this log directive:
-# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" ::: %{mod_php_memory_usage}n %D %O %v %U %q" combined-php
-# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" ::: %{forensic-id}n %{mod_php_memory_usage}n %D %O http://%v%U%q" combined-php-forensics
-# 127.0.0.1 - - [14/Dec/2012:14:56:58 -0500] "OPTIONS * HTTP/1.0" 200 - "-" "Apache/2.4.3 (Unix) OpenSSL/1.0.1c PHP/5.4.7 (internal dummy connection)" ::: - 1256 148 ushahidi-fresh.march-hare.org *
-# %h - Remote Host
-# %u - Remote user (from auth; may be bogus if return status (%s) is 401)
-# %t - Time the request was received (standard english format)
-# "%r" - First line of request
-# %>s - Status. For requests that got internally redirected, this is the status of the *original* request --- %>s for the last.
-# %b - Size of response in bytes, excluding HTTP headers. In CLF format, i.e. a '-' rather than a 0 when no bytes are sent.
-# %D - The time taken to serve the request, in microseconds.
-# %O - Bytes sent, including headers, cannot be zero. You need to enable mod_logio to use this.
-# %v - vhost
-# %U - url
-# %q - query string
 my $sth = $DBH->prepare('INSERT INTO logentries(remote_host, time, server_name, tts, size, url, params, memory) values(?, ?, ?, ?, ?, ?, ?, ?)')
     || die "$DBI::errstr";
 
@@ -87,50 +74,15 @@ my $sth = $DBH->prepare('INSERT INTO logentries(remote_host, time, server_name, 
 my $lang = Date::Language->new('English');
 my ($remote_host, $time, $servername, $tts, $size, $url, $params, $memory);
 while (<STDIN>) {
-	# pull in fields of interest from a RE
-	/
-		([^\s]+)						# %h - Remote Host
-		\s
-		(?:[^\s]+\s){2}			# %l %u
-		\[([^\]]+)\]				# %t - Time the request was recieved
-		\s
-		"[^\"]+"						# %r - First line of request
-		\s
-		(?:[^\s]+)					# %>s - Status
-		\s
-		([^\s]+)						# %b - Size of response
-		.*\s:::							# Everything up to the ':::' divider
-		
-		(?:
-			\s
-			([^\s]+)					# %{mod_php_memory_usage}n
-			\s
-			([^\s]+)					# %D - tts
-			\s
-			([^\s]+)					# %O - bytes sent
-			\s
-			([^\s]+)					# %v - vhost
-			\s
-			([^\s]+)					# %U - url
-			(?:
-				\s
-				([^\s]+)				# %q - params
-			)?
-		)?								# This whole section may not exist
-	/x or next;
-	$remote_host = $1;
-	$time = strftime "%F %H:%M:%S", localtime($lang->str2time($2));
-	$memory = ($4 eq "-")?'':$4;
-	$tts = $5;
-	$size = ($6 && ($6 ne "-"))?$6:$3;
-	$servername = $7;
-	$url = ($8 && ($8 ne '*'))?$8:'';
-	$params = $9;
-	print "$_\n\$remote_host =>$remote_host\n\$time => $time\n".
-          "\$servername => $servername\n\$tts => $tts\n\$size => $size\n".
-          "\$url => $url\n\$params => $params\n\$memory => $memory\n\n"
-          if $VERBOSE;
-	$sth->execute($remote_host, $time, $servername, $tts, $size, $url, $params, $memory);
+  my ($remote_host, $time, $memory, $tts, $size, $servername, $url, $params);
+  if ($FORMAT eq 'combined') {
+    ($remote_host, $time, $memory, $tts, $size, $servername, $url, $params) = parse_combined($_);
+  } elsif ($FORMAT eq 'combined-php') {
+    ($remote_host, $time, $memory, $tts, $size, $servername, $url, $params) = parse_combined_php($_);
+  } elsif ($FORMAT eq 'combined-php-forensics') {
+    ($remote_host, $time, $memory, $tts, $size, $servername, $url, $params) = parse_combined_php_forensics($_);
+  }
+  $sth->execute($remote_host, $time, $servername, $tts, $size, $url, $params, $memory);
 }
 
 # Close the database connection
@@ -206,7 +158,7 @@ sub install {
 			memory INT,
 
 			PRIMARY KEY(id),
-			INDEX(remote_host, server_name, url)			
+			INDEX(remote_host, server_name, url, time)			
 	)");
 	$sth->execute();
 	$sth->finish();
@@ -283,4 +235,179 @@ sub report {
 		print $row->{server_name}
 			."\ttts: ". $row->{tts} ."\tsize: ". $row->{size} ."\tmemory: ". $row->{memory} ."\n";
 	}
+}
+
+# We will be using this log directive:
+# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" ::: %{mod_php_memory_usage}n %D %O %v %U %q" combined-php
+# 127.0.0.1 - - [14/Dec/2012:14:56:58 -0500] "OPTIONS * HTTP/1.0" 200 - "-" "Apache/2.4.3 (Unix) OpenSSL/1.0.1c PHP/5.4.7 (internal dummy connection)" ::: - 1256 148 ushahidi-fresh.march-hare.org *
+# %h - Remote Host
+# %u - Remote user (from auth; may be bogus if return status (%s) is 401)
+# %t - Time the request was received (standard english format)
+# "%r" - First line of request
+# %>s - Status. For requests that got internally redirected, this is the status of the *original* request --- %>s for the last.
+# %b - Size of response in bytes, excluding HTTP headers. In CLF format, i.e. a '-' rather than a 0 when no bytes are sent.
+# %D - The time taken to serve the request, in microseconds.
+# %O - Bytes sent, including headers, cannot be zero. You need to enable mod_logio to use this.
+# %v - vhost
+# %U - url
+# %q - query string
+sub parse_combined_php {
+  my ($line) = @_;
+  # pull in fields of interest from a RE
+  $line =~ /
+          ([^\s]+)						# %h - Remote Host
+          \s
+          (?:[^\s]+\s){2}			# %l %u
+          \[([^\]]+)\]				# %t - Time the request was recieved
+          \s
+          "[^\"]+"						# %r - First line of request
+          \s
+          (?:[^\s]+)					# %>s - Status
+          \s
+          ([^\s]+)						# %b - Size of response
+          .*\s:::							# Everything up to the ':::' divider
+          
+          (?:
+                  \s
+                  ([^\s]+)					# %{mod_php_memory_usage}n
+                  \s
+                  ([^\s]+)					# %D - tts
+                  \s
+                  ([^\s]+)					# %O - bytes sent
+                  \s
+                  ([^\s]+)					# %v - vhost
+                  \s
+                  ([^\s]+)					# %U - url
+                  (?:
+                          \s
+                          ([^\s]+)				# %q - params
+                  )?
+          )?								# This whole section may not exist
+  /x or next;
+  $remote_host = $1;
+  $time = strftime "%F %H:%M:%S", localtime($lang->str2time($2));
+  $memory = ($4 eq "-")?'':$4;
+  $tts = $5;
+  $size = ($6 && ($6 ne "-"))?$6:$3;
+  $servername = $7;
+  $url = ($8 && ($8 ne '*'))?$8:'';
+  $params = $9;
+  print "$_\n\$remote_host =>$remote_host\n\$time => $time\n".
+    "\$servername => $servername\n\$tts => $tts\n\$size => $size\n".
+    "\$url => $url\n\$params => $params\n\$memory => $memory\n\n"
+    if $VERBOSE;
+  return ($remote_host, $time, $memory, $tts, $size, $servername, $url, $params);
+}
+
+# We will be using this log directive:
+# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+# 67.180.8.172 - - [02/Jul/2009:22:46:32 +0000] "GET / HTTP/1.1" 200 53 "-" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.11) Gecko/2009060214 Firefox/3.0.11"
+# %h - Remote Host
+# %l - Remote logname (from identd, if supplied). This will return a dash unless mod_ident is present and IdentityCheck is set On.
+# %u - Remote user (from auth; may be bogus if return status (%s) is 401)
+# %t - Time the request was received (standard english format)
+# "%r" - First line of request
+# %>s - Status. For requests that got internally redirected, this is the status of the *original* request --- %>s for the last.
+# %b - Size of response in bytes, excluding HTTP headers. In CLF format, i.e. a '-' rather than a 0 when no bytes are sent.
+# %D - The time taken to serve the request, in microseconds.
+# %{Referer}i - The Referer header from the client
+# %{User-Agent}i - The User-Agent header ffrom the client
+sub parse_combined {
+  my ($line) = @_;
+  # pull in fields of interest from a RE
+  $line =~ /
+    ([^\s]+)				# %h - Remote Host
+    \s
+    (?:[^\s]+\s){2}			# %l %u
+    \[([^\]]+)\]			# %t - Time the request was recieved
+    \s
+    "([^\"]+)"				# %r - First line of request
+    \s
+    (?:[^\s]+)				# %>s - Status
+    \s
+    ([^\s]+)				# %b - Size of response
+  /x or next;
+  $remote_host = $1;
+  $time = strftime "%F %H:%M:%S", localtime($lang->str2time($2));
+  $memory = '';
+  $tts = '';
+  $size = $4;
+  # TODO: this does not make a whole lot of sense
+  $servername = $SERVER;
+  $url = $3;
+  $params = '';
+  print "$_\n\$remote_host =>$remote_host\n\$time => $time\n".
+    "\$servername => $servername\n\$tts => $tts\n\$size => $size\n".
+    "\$url => $url\n\$params => $params\n\$memory => $memory\n\n"
+    if $VERBOSE;
+  return ($remote_host, $time, $memory, $tts, $size, $servername, $url, $params);
+}
+
+# We will be using this log directive:
+# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" ::: %{forensic-id}n %{mod_php_memory_usage}n %D %O http://%v%U%q" combined-php-forensics
+# 67.207.151.21 - - [13/Sep/2013:10:19:36 -0700] "GET / HTTP/1.1" 200 4057 "http://208.78.97.143" "WWW-Mechanize/1.64" ::: 24489:52334928:5c 786432 1746 4276 http://compare50.org/index.php
+# %h - Remote Host
+# %l - Remote logname (from identd, if supplied). This will return a dash unless mod_ident is present and IdentityCheck is set On.
+# %u - Remote user (from auth; may be bogus if return status (%s) is 401)
+# %t - Time the request was received (standard english format)
+# "%r" - First line of request
+# %>s - Status. For requests that got internally redirected, this is the status of the *original* request --- %>s for the last.
+# %b - Size of response in bytes, excluding HTTP headers. In CLF format, i.e. a '-' rather than a 0 when no bytes are sent.
+# %{Referer}i - The Referer header from the client
+# %{User-Agent}i - The User-Agent header ffrom the client
+# %{forensic-id}n - The unique forensics id assigned by mod_forensics
+# %{mod_php_memory_usage}n - The amount of memory used by php
+# %D - The time taken to serve the request, in microseconds.
+# %O - Bytes sent, including headers, cannot be zero. You need to enable mod_logio to use this.
+# http://%v%U%q - A string representing the requested url where:
+#   %v - vhost
+#   %U - url
+#   %q - query string
+sub parse_combined_php_forensics {
+  my ($line) = @_;
+  # pull in fields of interest from a RE
+  $line =~ /
+          ([^\s]+)						# %h - Remote Host
+          \s
+          (?:[^\s]+\s){2}			# %l %u
+          \[([^\]]+)\]				# %t - Time the request was recieved, $2
+          \s
+          "[^\"]+"						# %r - First line of request
+          \s
+          (?:[^\s]+)					# %>s - Status
+          \s
+          ([^\s]+)						# %b - Size of response, $3
+          .*\s:::							# Everything up to the ':::' divider
+          
+          (?:
+                  \s
+                  [^\s]+					# %{forensic-id}n
+                  \s
+                  ([^\s]+)					# %{mod_php_memory_usage}n, $4
+                  \s
+                  ([^\s]+)					# %D - tts, $5
+                  \s
+                  ([^\s]+)					# %O - bytes sent, $6
+                  \s
+                  ([^\s]+)					# http:FOO, $7
+          )?								# This whole section may not exist
+  /x or next;
+  my $remote_host = $1;
+  my $time = strftime "%F %H:%M:%S", localtime($lang->str2time($2));
+  my $memory = ($4 eq "-")?'':$4;
+  my $tts = $5;
+  my $size = ($6 && ($6 ne "-"))?$6:$3;
+  my $url = $7;
+  my $servername = '';
+  my $params = '';
+  if ($url =~ /http:\/\/([^\/]*)(?:\/([^?]*)(?:\?(.*))?)?/) {
+    $servername = $1;
+    $url = $2;
+    $params = $3;
+  }
+  print "$_\n\$remote_host =>$remote_host\n\$time => $time\n".
+    "\$servername => $servername\n\$tts => $tts\n\$size => $size\n".
+    "\$url => $url\n\$params => $params\n\$memory => $memory\n\n"
+    if $VERBOSE;
+  return ($remote_host, $time, $memory, $tts, $size, $servername, $url, $params);
 }
